d1a54d36fe3c4b85c6d0e4e72007c949e68841fd
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-02-04 22:55:07 +0530

    notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,55 +2,11 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
-import Card from './Card'
-// import contacts from '../contact'
-import emojis from '../contact'
+import notes from '../notes'
 
 
-// var numbers = [3,56,2,48,5];
-
-// map
-// const a = numbers.map(function (x){
-//   return x*2;
-// });
-// console.log(a)
 
 
-//filter
-
-// const a= numbers.filter(function (num){
-//   return num>10
-// })
-
-// console.log(a)
-
-
-// reduce
-
-// const a = numbers.reduce(function (acc,curr){
-//   console.log("acc = " + acc)
-//   console.log("curr = " +curr)
-//   return acc+curr;
-// })
-// console.log(a)
-
-
-// first item greater than 10
-// const a= numbers.find(num =>{
-//   return num>10
-// })
-// console.log(a)
-
-
-// function createCard(emoji){
-//   return <Card 
-//   key ={emoji.id}
-//   name={emoji.name} 
-//   img = {emoji.imgURL}
-//   tel = {emoji.meaning}
-//   email = {emoji.code}
-//   />
-// }
 
 export default function App() {
 
@@ -59,17 +15,12 @@ export default function App() {
       <Header />
 
       <div className='allCard'>
-      <Note />
 
-      {emojis.map(emoji => (
-        <Card 
-        key ={emoji.id} 
-        name={emoji.name} 
-        img = {emoji.imgURL} 
-        tel = {emoji.meaning} 
-        email = {emoji.code} 
-        />
-      ))}
+
+    {notes.map(note =>
+      <Note key={note.key} title={note.title} content={note.content} />
+    )}
+
 
       </div>
       <Footer />
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Note() {
+export default function Note(props) {
   return (
     <div className='note'>
-        <h1>This is Titel</h1>
-        <p>This is content</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
   )
 }
