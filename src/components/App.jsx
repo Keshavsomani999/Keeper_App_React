7b514d18d9c929801c976982915559b78fdf2e35
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-02-04 21:25:28 +0530

    emoji meaning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,35 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
+import Card from './Card'
+// import contacts from '../contact'
+import emojis from '../contact'
+
+
+
+
+function createCard(emoji){
+  return <Card 
+  key ={emoji.id}
+  name={emoji.name} 
+  img = {emoji.imgURL}
+  tel = {emoji.meaning}
+  email = {emoji.code}
+  />
+}
 
 export default function App() {
+
   return (
     <div>
       <Header />
+
+      <div className='allCard'>
       <Note />
+
+      {emojis.map(createCard)}
+
+      </div>
       <Footer />
     </div>
   )
