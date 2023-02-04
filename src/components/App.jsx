59a59ae91619d9094e3381f131f5595b84fc44ab
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-02-05 00:11:48 +0530

    isUserRegistered

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,12 @@ import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
 import notes from '../notes'
+import Login from './Login'
+import Form from './Form'
 
+var isUserRegistered = true
 
-
-
+const time = new Date(2019,11,1,18).getHours();
 
 export default function App() {
 
@@ -14,15 +16,19 @@ export default function App() {
     <div>
       <Header />
 
-      <div className='allCard'>
+      {isUserRegistered ? <Login /> : <Form /> }
+
+      {/* {time > 12 ? <h1> why still working</h1> : null} */}
+
+      {/* <div className='allCard'>
 
 
-    {notes.map(note =>
-      <Note key={note.key} title={note.title} content={note.content} />
-    )}
+          {notes.map(note =>
+            <Note key={note.key} title={note.title} content={note.content} />
+          )}
 
 
-      </div>
+      </div> */}
       <Footer />
     </div>
   )
---
+++ src/contact.js
@@ -0,0 +1,45 @@
+const emojis = [
+  {
+    id: 1,
+    name: "Smiling Face With Open Mouth",
+    imgURL:
+      "https://www.emojimeanings.net/img/emojis/smiling-face-with-open-mouth_1f603.png",
+    meaning:"Typical smiley face with open mouth and oval eyes. Is in a positive mood, shows its teeth and laughs cheerfully. Expresses enthusiasm: from a cheerful greeting to boundless joy.",
+    code:"U+1F603"
+},
+{
+    id: 2,
+    name: "Smiling Face With Open Mouth And Smiling Eyes",
+    imgURL:
+      "https://www.emojimeanings.net/img/emojis/smiling-face-with-open-mouth-and-smiling-eyes_1f604.png",
+    meaning:"Smiley's mouth is wide open, its eyes squeezed shut with joy. Laughs loudly, cheerfully and heartily. Only the typical emoji eyes distinguish it from the classical smiley face. Can also be used for sarcasm.",
+    code:"U+1F604"
+},
+{
+    id: 3,
+    name: "Grinning Face With Smiling Eyes",
+    imgURL:
+      "https://www.emojimeanings.net/img/emojis/grinning-face-with-smiling-eyes_1f601.png",
+    meaning:"Happy face with a mischievous laugh. Represents lightheartedness and exuberance. Full of joy due to an event, excited and agitated or just a bit embarrassed..",
+    code:"U+1F601"
+},
+{
+    id: 4,
+    name: "Smiling Face With Open Mouth And Closed Eyes",
+    imgURL:
+      "https://www.emojimeanings.net/img/emojis/smiling-face-with-open-mouth-and-tightly-closed-eyes_1f606.png",
+    meaning:"Emoji is on the verge of a fit of laughter because something is so silly or incredibly funny. Also, mischievous or bitchy laughter about an event, or something that happened to another person.. Means the same as “XD“ and “X“ as a text-based emoticon.",
+    code:"U+1F606"
+},
+{
+    id: 5,
+    name: "Face Holding Back Tears",
+    imgURL:
+      "https://www.emojimeanings.net/img/emojis/face-holding-back-tears_1f979.png",
+    meaning:"Slightly smiling face with tears in the eyes. It can express gratitude, emotion or pride. At the same time, however, anger or rage could be the reason for barely being able to hold back the tears. The emotional interpretation depends on the context of the communication.",
+    code:"U+1F979"
+},
+  
+];
+
+export default emojis;
