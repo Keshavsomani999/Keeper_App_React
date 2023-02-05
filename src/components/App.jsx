f083e18c68f9deff67fe840b3a6c0fb464fa237e
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-02-05 18:46:32 +0530

    change color of button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React,{ useState ,useEffect } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
@@ -8,66 +8,45 @@ import Form from './Form'
 import cars from './practice'
 
 
-const [honda,tesla] = cars
-
-const {speedStats:{topSpeed:hondaTopSpeed}} = honda
-const {speedStats:{topSpeed:teslaTopSpeed}} = tesla
-
-const {coloursByPopularity:[hondaTopColour]} = honda
-const {coloursByPopularity:[teslaTopColour]} = tesla
 
 export default function App() {
 
-  setInterval(inc,1000)
 
-  let getTime = new Date().toLocaleTimeString();
+const [headingText, setHeadingText] = useState("Hello");
 
-  const [time,setCount] = useState(getTime);
 
-  function inc(){
-    let newTime = new Date().toLocaleTimeString();
+const [useStyle,setStyle] = useState("black");
 
-    setCount(newTime);
-  }
 
+function handleClick(){
+
+  setHeadingText("Submited")
+}
+
+
+
+function handleMouseOver(){
+  setStyle("red")
+  
+}
+function handleMouseOut(){
+  document.body.style.backgroundColor="red"
+  setStyle("black")
+
+}
 
   return (
     <div>
       <Header />
 
-    <h1>{time}</h1>
-    <button onClick={inc}>Get Time</button>
-    {/* <button onClick={dec}>-</button> */}
 
-    <table>
-    <tr>
-      <th>Brand</th>
-      <th>Top Speed</th>
-      <th>Top Colour</th>
-    </tr>
-    <tr>
-      <td>{tesla.model}</td>
-      <td>{teslaTopSpeed}</td>
-      <td>{teslaTopColour}</td>
-    </tr>
-    <tr>
-      <td>{honda.model}</td>
-      <td>{hondaTopSpeed}</td>
-      <td>{hondaTopColour}</td>
-    </tr>
-  </table>
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button style={{backgroundColor:useStyle}}  onMouseOut={handleMouseOut} onMouseOver={handleMouseOver} onClick={handleClick}>Submit</button>
+      {/* <button style={{backgroundColor: isMouseOver ? "black" : "white"}} onClick={handleClick}>Submit</button> */}
+    </div>
 
-     
-
-      {/* <div className='allCard'>
-
-
-          {notes.map(note =>
-            <Note key={note.key} title={note.title} content={note.content} />
-          )}
-
-
-      </div> */}
       <Footer />
     </div>
   )
